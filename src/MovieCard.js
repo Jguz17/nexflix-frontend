efd562f1f38392606c4db54d1725bc749c3c68ce
jguz17 <johnathanguzman17@gmail.com>
2020-09-02 17:35:20 -0500

    finished. Need to make responsive

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import { Grid } from '@material-ui/core'
 
 function MovieCard(props) {
-    console.log(props)
     return (
             <img src={'https://image.tmdb.org/t/p/w500' + props.movie.poster_path}/>
         )
